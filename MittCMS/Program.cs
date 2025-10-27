dc93620adffbde89a40f4d6b86da30ce863b40f3
Kevin <41475249+Kevinswardh@users.noreply.github.com>
2025-10-27 09:53:20 +0100

    bugg?

--- MittCMS/Program.cs
+++ MittCMS/Program.cs
@@ -1,16 +1,14 @@
-using Azure.Identity;
+ï»¿using Azure.Identity;
 using Azure.Extensions.AspNetCore.Configuration.Secrets;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
-// === Lägg till Azure Key Vault ===
-// Detta gör att appen hämtar hemligheter (t.ex. umbracoDbDSN) direkt från ditt Key Vault
-builder.Configuration.AddAzureKeyVault(
-    new Uri("https://mittcms-keys.vault.azure.net/"),
-    new DefaultAzureCredential());
 
-// === Skapa Umbraco ===
+    builder.Configuration.AddAzureKeyVault(
+        new Uri("https://mittcms-keys.vault.azure.net/"),
+        new DefaultAzureCredential());
+
+
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
@@ -19,10 +17,8 @@ builder.CreateUmbracoBuilder()
 
 var app = builder.Build();
 
-// === Starta Umbraco ===
 await app.BootUmbracoAsync();
 
-// === Middleware ===
 app.UseHttpsRedirection();
 
 app.UseUmbraco()
